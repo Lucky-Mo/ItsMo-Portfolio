e69879debaf2bc9202788e101f96995db284dcd0
Lucky-Mo <mdothman06@outlook.com>
2025-12-12 11:35:08 +0100

    Update CSS styles and enhance HTML structure for header, home, and about sections; add new logo image

--- index.php
+++ index.php
@@ -8,37 +8,219 @@
     <title>Welcome to Its_Mo-Portfolio</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="image/png" href="images/portredlogo.png">
 </head>
 </head>
 <body>
     <div id="header">
         <div class="container">
             <nav>
-                <img src="images/portlogor.png" class="logo">
+                <img href='#' src="images/portlogor.png" class="logo">
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
+            <!-- Begin Home Section -->
             <div class="header-text">
-                <div class="home-content" data-sr-id="0" style="visibility: visible; opacity: 1; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transition: all, opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s;">
-                    <h3>Hello,It's Me</h3>
-                    <h1 data-sr-id="15" style="visibility: visible; opacity: 1; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transition: all, opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s;">Alizeh</h1>
-                    <h3>And I'm a <span class="multiple-text">Canva </span><span class="typed-cursor" aria-hidden="true">|</span></h3>
-                    <div class="social-media">
-                        <a href="#"><i class="bx bxl-github"></i></a>
-                        <a href="#"><i class="bx bxl-youtube"></i></a>
-                        <a href="#"><i class="bx bxl-instagram"></i></a>
-                        <a href="#"><i class="bx bxl-linkedin"></i></a>
-                    </div>
-                    <a href="#" class="btn">Download CV</a>
+                <h1>Make Your Business Grow Faster</h1>
+                <h3>I'm <span class="red">Muhammad</span></h3>
+                <h3>From <span class="green">S<span class="red">y</span>r<span class="red">i</span>a</span></h3>
+                <p>And I'm A Web Developer</p>
+                <div class="social-media">
+                    <a href="https://github.com/Lucky-Mo"><i class="bx bxl-github"></i></a>
+                    <a href="https://www.facebook.com/mohammad.othman.06/"><i class="bx bxl-facebook"></i></a>
+                    <a href="https://www.instagram.com/mootje_othman"><i class="bx bxl-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/muhammad-alothman-2155252b0"><i class="bx bxl-linkedin"></i></a>
                 </div>
             </div>
         </div>
     </div>
+             <!-- End Home Section -->
+
+             <!-- Begin About Section -->
+    <div id="about">
+        <div class="container">
+            <div class="row">
+                <div class="about-col-1">
+                    <img src="images/portimg.png">
+                </div>
+                <div class="about-col-2">
+                    <h1 class="sub-title">About Me</h1>
+                    <p>iuEHFBLIUEWHF IUAWLEIUFBIULSA BDEFVLIUBS DLIUFVBCLID BVFCLIUbsadvli uBsldiubvLIUBS UBVD UISBDv UIBsdlivu BSibdVIB SDVUIBSvdlUIBsdvbUIB VBUsi vUIls bvIUBvsviuBivsudb sd vbiu</p>
+                    <div class="tab-titles">
+                        <p class="tab-links active-link" onclick="opentab('skills')">Skills</p>
+                        <p class="tab-links" onclick="opentab('experience')">Experience</p>
+                        <p class="tab-links" onclick="opentab('education')">Education</p>
+                    </div>
+
+                    <div class="tab-contents active-tabs" id="skills">
+                        <ul>
+                            <li><span>Web Development </span>Web App Development</li>
+                            <li><span>App Development </span>Designing web/app interfaces</li>
+                            <li><span>Editing </span>Editing Photos & Videos</li>
+                        </ul>
+                    </div>
+
+                    <div class="tab-contents" id="experience">
+                        <ul>
+                            <li>
+                                <span>2027 - Current</span><br>
+                                Graduated from Vista College with a diploma in Software Development.
+                            </li>
+                            <li>
+                                <span>2026 - 2027</span><br>
+                                Web Development Training/Internship at Code Academy
+                            </li>
+                            <li>
+                                <span>2025 - 2026</span><br>
+                                Same as last year.
+                            </li>
+                            <li>
+                                <span>2024 - 2028</span><br>
+                                Studied Software Development at Vista-College in The Netherlands for 4 years. 
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div class="tab-contents" id="education">
+                        <ul>
+                            <li>
+                                <span>2027 - Current</span><br>
+                                Graduated from Vista College with a diploma in Software Development.
+                            </li>
+                            <li>
+                                <span>2026 - 2027</span><br>
+                                Web Development Training/Internship at Code Academy
+                            </li>
+                            <li>
+                                <span>2025 - 2026</span><br>
+                                Same as last year.
+                            </li>
+                            <li>
+                                <span>2024 - 2025</span><br>
+                                Started studying Software Development at Vista-College in The Netherlands. 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <!-- End About Section -->
+
         
+
+        <!-- Begin JavaScript -->
+        <script>
+
+            var tablinks = document.getElementsByClassName("tab-links");
+            var tabcontents = document.getElementsByClassName("tab-contents");
+
+            function opentab(tabname){
+                for(tablink of tablinks){
+                    tablink.classList.remove("active-link");
+                }
+                
+                for(tabcontent of tabcontents){
+                    tabcontent.classList.remove("active-tab");
+                }
+                
+                event.currentTarget.classList.add("active-link");
+                document.getElementById(tabname).classList.add("active-tab");
+            }
+        </script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+<!-- Begin Services Section 
+        <div id="services">
+            <div class="container">
+                <h1 class="sub-title">My Services</h1>
+                <div class="row">
+                    <div class="service-col">
+                        <h3>Web Development</h3>
+                        <p>Creating responsive and functional websites.</p>
+                    </div>
+                    <div class="service-col">
+                        <h3>App Development</h3>
+                        <p>Building user-friendly mobile applications.</p>
+                    </div>
+                    <div class="service-col">
+                        <h3>UI/UX Design</h3>
+                        <p>Designing intuitive interfaces for better user experience.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Services Section -->
+
+        <!-- Begin Portfolio Section 
+        <div id="portfolio">
+            <div class="container">
+            <h1 class="sub-title">My Portfolio</h1>
+            <div class="row">
+                <div class="portfolio-col">
+                <img src="images/project1.png" alt="Project 1">
+                <h3>Project Title 1</h3>
+                <p>Brief description of the project.</p>
+                </div>
+                <div class="portfolio-col">
+                <img src="images/project2.png" alt="Project 2">
+                <h3>Project Title 2</h3>
+                <p>Brief description of the project.</p>
+                </div>
+                <div class="portfolio-col">
+                <img src="images/project3.png" alt="Project 3">
+                <h3>Project Title 3</h3>
+                <p>Brief description of the project.</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- End Portfolio Section -->
+
+        <!-- Begin Contact Section 
+        <div id="contact">
+            <div class="container">
+            <h1 class="sub-title">Contact Me</h1>
+            <form action="send_message.php" method="POST">
+                <input type="text" name="name" placeholder="Your Name" required>
+                <input type="email" name="email" placeholder="Your Email" required>
+                <textarea name="message" placeholder="Your Message" required></textarea>
+                <button type="submit">Send Message</button>
+            </form>
+            </div>
+        </div>
+        <!-- End Contact Section -->
+
+
+
+
+
+
+<!-- 
+ <div class="home-content" data-sr-id="0" style="visibility: visible; opacity: 1; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transition: all, opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s;">
+                    <h3>Hello,It's Me</h3>
+                    <h1 data-sr-id="15" style="visibility: visible; opacity: 1; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transition: all, opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s;">Muhammad Alothman</h1>
+                    <h3>And I'm a <span class="multiple-text">Web Developer</span><span class="typed-cursor" aria-hidden="true">|</span></h3>
+                    <div class="social-media">
+                        <a href="https://github.com/Lucky-Mo"><i class="bx bxl-github"></i></a>
+                        <a href="https://www.facebook.com/mohammad.othman.06/"><i class="bx bxl-facebook"></i></a>
+                        <a href="https://www.instagram.com/mootje_othman"><i class="bx bxl-instagram"></i></a>
+                        <a href="https://www.linkedin.com/in/muhammad-alothman-2155252b0"><i class="bx bxl-linkedin"></i></a>
+                    </div>
+                    <a href="#" class="btn">Download My CV</a>
+                </div>
+ -->
